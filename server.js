dd379dca8e4f4ad57e96edc31ab3fe7d471f463d
jackie <mvpbremah@yahoo.com>
2020-10-18 17:15:23 -0400

    commit

--- server.js
+++ server.js
@@ -1,43 +1,24 @@
-const express = require('express');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const routes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
-
-require('dotenv').config()
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 
-
-app.use(logger('dev'));
-
-
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-app.use(express.static('public'));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
+const db = require("./models");
 
-
-
-
-
-app.use('/api', routes);
-app.use('/', htmlRoutes);
-
-mongoose.connect(
-	process.env.MONGODB_URI || 'mongodb://localhost/workout',
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-		useFindAndModify: false
-	}
-);
-
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
-	console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
+
 });
