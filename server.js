1b81c7e8137988919dee4662435aee78a6c67e53
jackie <mvpbremah@yahoo.com>
2020-10-18 17:43:47 -0400

    commit

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const apiRoutes = require("./routes/apiRoute.js");
+const htmlRoutes = require("./routes/htmlRoute.js")
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(morgan("dev"));
 
-app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
-const db = require("./models");
-
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.use(htmlRoutes);
+app.use(apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
-
 });
