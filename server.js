dc4ce363ab9d46fc454e9620a1c306bd8d46cc0e
jackie <mvpbremah@yahoo.com>
2020-10-18 18:04:33 -0400

    commit

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+}
+);
 
 app.use(htmlRoutes);
 app.use(apiRoutes);
