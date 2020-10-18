b608eaab5316eafb8df84987905820d829617626
jackie <mvpbremah@yahoo.com>
2020-10-18 18:12:07 -0400

    commit

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const apiRoutes = require("./routes/apiRoutes.js");
-const htmlRoutes = require("./routes/htmlRoutes.js")
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const routes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+require('dotenv').config()
+
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
-app.use(morgan("dev"));
+
+
+app.use(logger('dev'));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+
+app.use(express.static('public'));
+
+
+app.use('/api', routes);
+app.use('/', htmlRoutes);
+
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
 	useNewUrlParser: true,
@@ -21,9 +32,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
 }
 );
 
-app.use(htmlRoutes);
-app.use(apiRoutes);
-
+// Listener
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+	console.log(`App running on port ${PORT}!`);
 });
