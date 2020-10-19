1833ca43bb003457a815ce0bcd8708183cdec431
jackie <mvpbremah@yahoo.com>
2020-10-18 20:29:59 -0400

    commit

--- server.js
+++ server.js
@@ -1,37 +1,40 @@
-//Require environment variables
-require("dotenv").config();
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const routes = require('./routes/index');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-const express = require("express");
-const mongoose = require("mongoose");
-const morgan = require('morgan');
+require('dotenv').config()
 
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 
-app.use(morgan('dev'));
+
+app.use(logger('dev'));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
+// Set up api routes
+app.use('/api', routes);
 
-const apiRoutes = require("./routes/apiRoutes");
-app.use("/api", apiRoutes);
+// Set up html routes
+app.use('/', htmlRoutes);
 
+// connect to Mongo database with Mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+}
+);
 
-const htmlRoutes = require("./routes/htmlRoutes");
-app.use("", htmlRoutes);
-
-mongoose.connect(process.env.MONGODB_URI || "", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
-
+// Listener
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+	console.log(`App running on port ${PORT}!`);
 });
